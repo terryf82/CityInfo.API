bfb8ddb9da1fc6774135d085fcb51a840ec44dc1
Terry Franklin <terryfranklin82@gmail.com>
2019-01-07 20:56:09 +1000

    POI creation with validation

--- Controllers/PointsOfInterestController.cs
+++ Controllers/PointsOfInterestController.cs
@@ -1,5 +1,7 @@
 using System.Linq;
+using CityInfo.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace CityInfo.API.Controllers
 {
@@ -17,7 +19,7 @@ namespace CityInfo.API.Controllers
             return Ok(city.PointsOfInterest);
         }
 
-        [HttpGet("api/cities/{cityId}/pointsofinterest/{poiId}")]
+        [HttpGet("api/cities/{cityId}/pointsofinterest/{poiId}", Name = "GetPointOfInterest")]
         public IActionResult GetPointOfInterest(int cityId, int poiId)
         {
             var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
@@ -35,6 +37,48 @@ namespace CityInfo.API.Controllers
             }
 
             return Ok(poi);
+        }
+
+        [HttpPost("api/cities/{cityId}/pointsofinterest")]
+        public IActionResult CreatePointOfInterest(int cityId,
+            [FromBody] PointOfInterestForCreationDto pointOfInterest)
+        {
+            if (pointOfInterest == null)
+            {
+                return BadRequest();
+            }
+
+            if (pointOfInterest.Name == pointOfInterest.Description)
+            {
+                ModelState.AddModelError("Description", "Name and description cannot be the same");
+            }
+
+            if (ModelState.IsValid == false)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(
+                c => c.PointsOfInterest).Max(p => p.Id);
+
+            var finalPointOfInterest = new PointOfInterestDto()
+            {
+                Id = ++maxPointOfInterestId,
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description
+            };
+
+            city.PointsOfInterest.Add(finalPointOfInterest);
+
+            return CreatedAtRoute("GetPointOfInterest",
+                new {cityId = city.Id, poiId = finalPointOfInterest.Id}, finalPointOfInterest);
         }
     }
 }
---
+++ Models/PointOfInterestForCreationDto.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.API.Models
+{
+    public class PointOfInterestForCreationDto
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
+        [MaxLength(200)]
+        public string Description { get; set; }
+    }
+}
